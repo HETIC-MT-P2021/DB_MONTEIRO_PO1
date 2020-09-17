10e1a68b34fa48016f16d03b2cae76193bb70af1
Wyllis Monteiro <wyllismonteiro@gmail.com>
2020-09-17 10:41:45 +0200

    feat: init router

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wyllisMonteiro/DB_MONTEIRO_P01
+
+go 1.14
+
+require github.com/gorilla/mux v1.8.0
---
+++ router/router.go
@@ -0,0 +1,10 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+)
+
+// Load controller (handler)
+func InitRoutes(r *mux.Router) *mux.Router {
+	return r
+}
