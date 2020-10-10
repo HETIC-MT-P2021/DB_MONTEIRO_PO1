d5b28b144244bff3e4035fc70ca0ef38c8a87582
wyllisMonteiro <wyllismonteiro@gmail.com>
2020-10-10 17:27:22 +0200

    feat: set up env var for db

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/wyllisMonteiro/DB_MONTEIRO_PO1/router"
 	"github.com/wyllisMonteiro/DB_MONTEIRO_PO1/models"
+	"github.com/wyllisMonteiro/DB_MONTEIRO_PO1/router"
 )
 
 func main() {
 	r := mux.NewRouter()
 	router.InitRoutes(r)
-	
+
 	models.ConnectToBDD()
-	
+
 	log.Fatal(http.ListenAndServe(":9000", r))
 }
